8533fac9040e614d27a7ce7c33292ecc28b74d4a
linggefanye <84019501+linggefanye@users.noreply.github.com>
2023-04-24 15:30:52 +0800

    Update generate.rs

--- src/generate.rs
+++ src/generate.rs
@@ -1,16 +1,35 @@
 use rand::{
-    Rng;|
-    rngs::StdRng;
-    SeedableRng;
-}
+    Rng,
+    rngs::StdRng,
+    SeedableRng,
+};
 
-#[derive(Debug)]
-struct Target {
+#[derive(Debug, Clone)]
+pub struct Target {
     syscalls: Vec<Syscall>,
     resources: Vec<Resource>,
 
     //...
 }
+
+#[derive(Debug, Clone)]
+pub struct Resource {
+    name: String,
+    base_type: BaseType,
+    consts: Vec<u64>,
+    type_options: Option<Vec<String>>,
+}
+
+#[derive(Debug, Clone)]
+enum BaseType {
+    Int8,
+    Int16,
+    Int32,
+    Int64,
+    IntPtr,
+    Custom(String),
+}
+
 
 #[derive(Debug, Clone)]
 pub struct Prog {
@@ -25,8 +44,8 @@ pub struct Call {
     //...
 }
 
-#[derive(Debug)]
-pub struct SysCall {
+#[derive(Debug, Clone)]
+pub struct Syscall {
     pub name: String,
     pub args: Vec<Field>,
 }
@@ -42,10 +61,11 @@ pub struct Field {
 pub struct Argument {
     typename: String,
     dir: Dir,
-    size: u64,
+    bitsize: u64,
+    val: u64,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Dir {
     DirIn,
     DirOut,
@@ -54,13 +74,14 @@ enum Dir {
 
 pub fn generate(target: &Target, rs: u64, ncalls: usize/* , ct: Option<&ChoiceTable> */) -> Prog {
     let mut p = Prog {
-        target: target,
+        target: target.clone(),
+        calls: Vec::new(),
     };
     let mut rng = StdRng::seed_from_u64(rs);
     //let s = State::new(target, ct, None);
 
     while p.calls.len() < ncalls {
-        let calls = generate_call(&mut rng, /* &s, */ &mut p, p.calls.len());
+        let calls = generate_call(&mut rng, /* &s, */ p.calls.len(), &mut p);
         for c in calls {
             /* s.analyze(c); */
             p.calls.push(c);
@@ -79,9 +100,127 @@ pub fn generate(target: &Target, rs: u64, ncalls: usize/* , ct: Option<&ChoiceTa
 fn generate_call<R: Rng>(
     rng: &mut R,
     /* s: &State, */
-    p: &mut Prog,
     insertion_point: usize,
+    p: &mut Prog,
 ) -> Vec<Call> {
-    // Implementation of generate_call goes here.
+    /* let mut bias_call = -1;
+    if insertion_point > 0 {
+        // Choosing the base call is based on the insertion point of the new calls sequence.
+        let insertion_call = &p.calls[rng.gen_range(0..insertion_point)].meta;
+        if !insertion_call.attrs.no_generate {
+            // We must be careful not to bias towards a non-generatable call.
+            bias_call = insertion_call.id;
+        }
+    } */
+    let syscall_count = p.target.syscalls.len(); // Replace with the appropriate method to get the number of syscalls
+    let idx = rng.gen_range(0..syscall_count);
+    let meta = &p.target.syscalls[idx];
+    generate_particular_call(rng,/*  s, */ meta)
+}
+
+fn generate_particular_call<R: Rng>(
+    rng: &mut R,
+   /*  s: &State, */
+    meta: &Syscall,
+) -> Vec<Call> {
+    /* if meta.attrs.disabled {
+        panic!("generating disabled call {}", meta.name);
+    }
+    if meta.attrs.no_generate {
+        panic!("generating no_generate call: {}", meta.name);
+    } */
+    
+    let mut c = make_call(meta, Vec::new());
+    let (args, mut calls) = generate_args(rng,/*  s,  */&meta.args, Dir::DirIn);
+    c.args = args;
+    /* s.target.assign_sizes_call(&mut c); */
+    calls.push(c);
+
+    calls
+}
+
+fn make_call(meta: &Syscall, args: Vec<Argument>) -> Call {
+    Call {
+        meta: meta.clone(),
+        args: args.clone(),
+    }
+}
+
+
+fn generate_args<R: Rng>(
+    rng: &mut R,
+    /* s: &State, */
+    fields: &Vec<Field>,
+    dir: Dir,
+) -> (Vec<Argument>, Vec<Call>) {
+    let mut calls = Vec::new();
+    let mut args = Vec::with_capacity(fields.len());
+
+    // Generate all args. Size args have the default value 0 for now.
+    for field in fields {
+        let (arg, mut calls1) = generate_arg(rng, &field.typename, dir.clone());
+        /* if arg.is_none() {
+            panic!(
+                "generated arg is nil for field '{}', fields: {:?}",
+                field.typename, fields
+            );
+        } */
+        args.push(arg);
+        calls.append(&mut calls1);
+    }
+
+    (args, calls)
+}
+
+// Stub implementation of generate_arg function
+fn generate_arg<R: Rng>(
+    rng: &mut R,
+    typename: &String,
+    dir: Dir,
+) -> (Argument, Vec<Call>) {
     // ...
+    match typename.as_str(){
+        "Int8" => (
+            Argument{
+                typename: "Int8".to_string(),
+                dir: dir,
+                bitsize: 8,
+                val: rng.gen::<u8>() as u64,
+            },
+            Vec::new()
+        ),
+        _ => panic!(),
+    }
+}
+
+
+
+fn main() {
+    let syscall = Syscall {
+        name: "syz_test_write".to_string(),
+        args: vec![
+            Field {
+                name: "count".to_string(),
+                typename: "Int8".to_string(),
+                type_options: Some(vec![]),
+            },
+        ],
+    };
+
+    let target = Target {
+        syscalls: vec![syscall.clone()],
+        resources: vec![],
+        // ...
+    };
+
+    let prog = generate(&target, 12345, 1);
+
+    println!("Generated Prog: {:#?}", prog);
+
+    // Test if the generated program has the expected syscall
+    for call in &prog.calls {
+        if call.meta.name == syscall.name {
+            println!("Found expected syscall: {}", syscall.name);
+        }
+    }
 }
